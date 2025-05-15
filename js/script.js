bdb45f91754aa38d752dfe010524f3071c35019a
Reginaldo da Rosa Correa Jr <reginaldodarosacorreajr@gmail.com>
2025-05-15 15:42:14 -0300

    terceiro commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const links = document.querySelectorAll(".header-menu a");
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  url.includes;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(parametro);
+}
+
+parametros.forEach(ativarProduto);
+
+
+
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
