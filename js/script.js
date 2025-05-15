94b6e9185574c67ddba40c266c1f150df142de4c
Reginaldo da Rosa Correa Jr <reginaldodarosacorreajr@gmail.com>
2025-05-15 16:38:00 -0300

    quarto

--- js/script.js
+++ js/script.js
@@ -41,3 +41,31 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+
+
+
+
+
+
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  galeriaContainer.prepend(img)
+
+}
+
+function eventosGaleria(img){
+  img.addEventListener("click", trocarImagem)
+}
+
+galeria.forEach(eventosGaleria);
+
+
+if(window.SimpleAnime){
+new SimpleAnime();
+}
